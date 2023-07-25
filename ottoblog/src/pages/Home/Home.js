363df9dcd1c9870757e4315b475b996618f64cfb
othongonzaga <othoncesar1@hotmail.com>
2023-07-24 22:06:28 -0300

    Estruturando página home

--- ottoblog/src/pages/Home/Home.js
+++ ottoblog/src/pages/Home/Home.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import styles from './Home.module.css'
 
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  };
+
   return (
-    <div>
-        <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
